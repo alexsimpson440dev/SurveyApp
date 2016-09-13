2929e372e3f13fa6637e3e99a6f4737dde2c63e5
Alex <alexsimpson440dev@gmail.com>
2016-09-13 03:31:58 -0500

    added the ability for the rotation to save how many results were clicked
    added the reset button to reset both counters back to zero

--- app/src/main/java/com/example/me5013zu/surveyapp/SurveyApp.java
+++ app/src/main/java/com/example/me5013zu/surveyapp/SurveyApp.java
@@ -2,16 +2,23 @@ package com.example.me5013zu.surveyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class SurveyApp extends AppCompatActivity {
 
+    //tags for logging
+    private static String TAG = "SurveyApp";
+    private static String KEY_INDEX_YES = "indexY";
+    private static String KEY_INDEX_NO = "indexN";
+
     //private int counters for each button.
     //will count the number of times the yes/no buttons were clicked
     private int yesButtonCounter = 0;
     private int noButtonCounter = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,7 @@ public class SurveyApp extends AppCompatActivity {
         Button mYesButton = (Button) findViewById(R.id.yes_button);
         Button mNoButton = (Button) findViewById(R.id.no_button);
         Button mResultsButton = (Button) findViewById(R.id.results_button);
+        Button mResetbutton = (Button) findViewById(R.id.reset_button);
 
         //button click method that adds one to the yes counter
         mYesButton.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +54,27 @@ public class SurveyApp extends AppCompatActivity {
                 Toast.makeText(SurveyApp.this, "Yes Total: " + yesButtonCounter + ", No Total: " + noButtonCounter, Toast.LENGTH_LONG).show();
             }
         });
+
+        //button click method that resets the counters
+        mResetbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                yesButtonCounter = 0;
+                noButtonCounter = 0;
+            }
+        });
+
+        if (savedInstanceState != null) {
+            yesButtonCounter = savedInstanceState.getInt(KEY_INDEX_YES, 0);
+            noButtonCounter = savedInstanceState.getInt(KEY_INDEX_NO, 0);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX_YES, yesButtonCounter);
+        savedInstanceState.putInt(KEY_INDEX_NO, noButtonCounter);
     }
 }
