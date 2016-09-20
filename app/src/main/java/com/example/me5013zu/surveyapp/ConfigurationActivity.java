c752e5cd560597c45153024035922e5a6cd26d0b
Alex <alexsimpson440dev@gmail.com>
2016-09-20 02:53:57 -0500

    started on the configuration activity (did not finish)

--- app/src/main/java/com/example/me5013zu/surveyapp/ConfigurationActivity.java
+++ app/src/main/java/com/example/me5013zu/surveyapp/ConfigurationActivity.java
@@ -2,6 +2,12 @@ package com.example.me5013zu.surveyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ConfigurationActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class ConfigurationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+
+        Button saveChanges = (Button) findViewById(R.id.save_changes_button);
+
+        saveChanges.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView question = (TextView) findViewById(R.id.textView);
+                TextView response1 = (TextView) findViewById(R.id.textView2);
+                TextView response2 = (TextView) findViewById(R.id.textView3);
+
+                EditText editQuestion = (EditText) findViewById(R.id.edit_question);
+                EditText editResponse1 = (EditText) findViewById(R.id.edit_response1);
+                EditText editResponse2 = (EditText) findViewById(R.id.edit_response2);
+
+                question.setText(editQuestion.getText());
+                //response1.setText(editResponse1.getText());
+                //response2.setText(editResponse2.getText());
+
+            }
+        });
     }
 }
